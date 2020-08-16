6a8d62d6350b6d7f9ad1c0d54e6b232951a919d7
onka13 <onurkaya13@live.com>
2020-08-16 11:37:36 +0300

    Fixed Android Fragment Type https://github.com/ChangeFinance/zendesk-flutter-plugin/issues/26

--- example/android/app/src/main/java/com/getchange/zendesk_flutter_plugin_example/MainActivity.java
+++ example/android/app/src/main/java/com/getchange/zendesk_flutter_plugin_example/MainActivity.java
@@ -2,7 +2,8 @@ package com.getchange.zendesk_flutter_plugin_example;
 
 import android.os.Bundle;
 //import io.flutter.app.FlutterActivity;
-import io.flutter.app.FlutterFragmentActivity;
+//import io.flutter.app.FlutterFragmentActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends /*FlutterActivity*/ FlutterFragmentActivity {

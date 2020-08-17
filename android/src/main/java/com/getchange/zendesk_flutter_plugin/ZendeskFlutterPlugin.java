625a035f513b3db02559d78d102f8d33a1f22619
onka <onurkaya13@gmail.com>
2020-08-17 13:58:29 +0300

    Update ZendeskFlutterPlugin.java

--- android/src/main/java/com/getchange/zendesk_flutter_plugin/ZendeskFlutterPlugin.java
+++ android/src/main/java/com/getchange/zendesk_flutter_plugin/ZendeskFlutterPlugin.java
@@ -24,7 +24,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.PluginRegistry;
-//import io.flutter.app.FlutterFragmentActivity;
 import io.flutter.embedding.android.FlutterFragmentActivity;
 
 import java.io.File;
